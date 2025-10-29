15d0991589849138dcb20e56798aac3bd158cfa0
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2025-10-29 22:20:35 +0100

    feat: Added eslint

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,44 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig } from 'eslint/config';
+import angular from "angular-eslint";
+
+export default defineConfig(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
