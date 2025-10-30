43e3776f8debabb5dba4ea4515f1a9fd2f8cfc94
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2025-10-30 15:12:41 +0100

    feat: Lazy loading routes in frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { Login as LoginComponent } from '@/app/security/components/login/login';
-import { Register as RegisterComponent } from '@/app/security/components/register/register';
-import { Chatbot as ChatbotComponent } from '@/app/chatbot/components/chatbot/chatbot';
 import { authenticated as authenticatedGuard } from '@/app/security/router/guards/authenticated';
 import { anonymous as anonymousGuard } from '@/app/security/router/guards/anonymous';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, canActivate: [anonymousGuard] },
-  { path: 'register', component: RegisterComponent, canActivate: [anonymousGuard] },
-  { path: 'chatbot', component: ChatbotComponent, canActivate: [authenticatedGuard] },
+  { path: 'login', loadComponent: () => import('@/app/security/components/login/login').then(m => m.Login), canActivate: [anonymousGuard] },
+  { path: 'register', loadComponent: () => import('@/app/security/components/register/register').then(m => m.Register), canActivate: [anonymousGuard] },
+  { path: 'chatbot', loadComponent: () => import('@/app/chatbot/components/chatbot/chatbot').then(m => m.Chatbot), canActivate: [authenticatedGuard] },
   { path: '**', redirectTo: '/login' }
 ];
